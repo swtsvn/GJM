ab2b89f254b66a1d07c6d8308803a77090c1f0df
SujathaNA <sujatha.1psg@gmail.com>
2016-12-06 06:16:56 -0600

    First master branch

---
+++ 2016 Dec/GJMDec/GJMDec/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GJMDec
+//
+//  Created by Sujatha Nagarajan on 12/6/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
